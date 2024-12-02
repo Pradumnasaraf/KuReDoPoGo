a715e16875c3dfd9cbe9fb5343ed1a80b744f458
Pradumna Saraf <pradumnasaraf@gmail.com>
2024-12-02 20:10:36 +0530

    feat: update README and migrate from Gin to Fiber framework; add PostgreSQL integration with product management functionality

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## KuReDoPoGo
 
-Kubernetes Redis Docker Postgres Golang - KuReDoPoGo, is an backend API service that is built using Golang ang Gin Framework. It uses Redis for rate limiting and Postgres for storing data. It is deployed on Kubernetes. For local development it uses Docker Compose.
+Kubernetes Redis Docker Postgres Golang - KuReDoPoGo, is an backend API service that is built using Golang and Fiber Framework. It uses Redis for rate limiting and Postgres for storing data. It is deployed on Kubernetes. For local development it uses Docker Compose.
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	random "github.com/Pallinder/go-randomdata"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type Product struct {
+	Name      string
+	Price     float64
+	Available bool
+}
 
 func main() {
 	err := godotenv.Load()
@@ -15,11 +23,98 @@ func main() {
 	} else {
 		log.Println("Using .env file")
 	}
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hey, I'm a server!",
-		})
-	})
-	log.Fatal(router.Run(":" + os.Getenv("PORT")))
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Connected to database")
+	}
+
+	// Create a product table
+	createProductTable(db)
+
+	// Insert a product
+	product := Product{
+		Name:      random.SillyName(),
+		Price:     random.Decimal(10, 100),
+		Available: random.Boolean(),
+	}
+	pk := insertProduct(db, product)
+	log.Println("Product created with id:", pk)
+
+	// Get a Product
+
+	//getProducts(db)
+
+	// Get all Products
+
+	//getAllProducts(db)
+}
+
+func createProductTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS product (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) NOT NULL,
+		price NUMERIC(6, 2) NOT NULL,
+		available BOOLEAN,
+		created Timestamp DEFAULT NOW()
+	)`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product Product) int {
+	query := `INSERT INTO product (name, price, available) VALUES ($1, $2, $3) RETURNING id`
+	var pk int
+	err := db.QueryRow(query, product.Name, product.Price, product.Available).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pk
+}
+
+func getProducts(db *sql.DB) {
+	var name string
+	var price float64
+	var available bool
+
+	query := `SELECT name, price, available FROM product WHERE id = $1`
+	pk := 1
+	err := db.QueryRow(query, pk).Scan(&name, &price, &available)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatal("No product found with id:", pk)
+		}
+		log.Fatal(err)
+	}
+	log.Println("Product:", name, price, available)
+}
+
+func getAllProducts(db *sql.DB) {
+	date := []Product{}
+	rows, err := db.Query("SELECT name, price, available FROM product")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		var price float64
+		var available bool
+		err := rows.Scan(&name, &price, &available)
+		if err != nil {
+			log.Fatal(err)
+		}
+		date = append(date, Product{name, price, available})
+	}
+	log.Println(date)
 }
